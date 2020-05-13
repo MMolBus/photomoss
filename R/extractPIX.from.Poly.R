daee708edc27cba9daa88dc041b4c846af8556af
Manuel <manuel.molbus@gmail.com>
2020-05-13 13:51:03 +0200

    title and brief description added

--- R/extractPIX.from.Poly.R
+++ R/extractPIX.from.Poly.R
@@ -1,15 +1,18 @@
-extractPIX.from.Poly <-
-function(tif.path, poly){
-      # file <- Sys.glob(path = paste0(tif.path, "vis/*.tif"))[1]
-      file <-list.files(path = "./vis",full.names = T)[1]
-      vis.tiff <- tiff::readTIFF(file)
-      vis.red <- raster::raster(vis.tiff[, , 1])
-
-      message("Extracting cells. Please wait.")
-      cells <- data.frame(raster::extract(vis.red, poly, cellnumbers = T))[, 
-                                                                   1]
-      out <- data.frame(cells, raster::rowColFromCell(vis.red, cells), 
-                        raster::xyFromCell(vis.red, cells))
-      return(out)
+# TITLE: extractPIX.from.Poly
+# Function: extract pixel positions enclosed by a polygon.
+extractPIX.from.Poly<- function(tif.path, poly){
+  # function(poly){
+  # file <- Sys.glob(path = paste0(tif.path, "vis/*.tif"))[1]
+  # file <-list.files(path = "./vis",full.names = T)
+  # file <- tif.path
+  # vis.tiff <- tiff::readTIFF(file)
+  vis.tiff <- tiff::readTIFF(tif.path)
+  vis.red <- raster::raster(vis.tiff[, , 1])
+  
+  message("Extracting cells. Please wait.")
+  cells <- data.frame(raster::extract(vis.red, poly, cellnumbers = T))[,1]
+  out <- data.frame(cells, raster::rowColFromCell(vis.red, cells), 
+                    raster::xyFromCell(vis.red, cells))
+  return(out)
 }
 
