fea064a021d56b4fe52ae3d2644000269be06aa3
Manuel <manuel.molbus@gmail.com>
2020-06-04 13:54:55 +0200

    eliminate tif path argument and set reference image with the first picture of the vis image set

--- R/extractPIX.from.Poly.R
+++ R/extractPIX.from.Poly.R
@@ -1,12 +1,12 @@
 # TITLE: extractPIX.from.Poly
 # Function: extract pixel positions enclosed by a polygon.
-extractPIX.from.Poly<- function(tif.path, poly){
+extractPIX.from.Poly<- function(poly){
   # function(poly){
-  # file <- Sys.glob(path = paste0(tif.path, "vis/*.tif"))[1]
+  # file <- Sys.glob(paths = paste0(tif.path, "vis/*.tif"))[1]
   # file <-list.files(path = "./vis",full.names = T)
   # file <- tif.path
   # vis.tiff <- tiff::readTIFF(file)
-  vis.tiff <- tiff::readTIFF(tif.path)
+  vis.tiff <- tiff::readTIFF(list.files("./vis", full.names = T)[1])
   vis.red <- raster::raster(vis.tiff[, , 1])
   
   message("Extracting cells. Please wait.")
