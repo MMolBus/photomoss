246dc457642dcbc96fd28bfa77ef32e52deddbff
MMolBus <36168270+MMolBus@users.noreply.github.com>
2023-01-09 20:04:27 +0100

    Create TSS.IoU.calc.R
    fujnction to calculate TSS and iou from list.results object calculated for a index

---
+++ R/TSS.IoU.calc.R
@@ -0,0 +1,23 @@
+# TSS and IoU calculator
+# list.result <- list.results[[1]][[i]]
+TSS.IoU.calc <-
+  function(list.result) {
+    
+    # TSS calculation
+    sensitivity <-
+      sum(list.result$TP) / (sum(list.result$TP) + sum(list.result$FN))
+    
+    specificity <-
+      sum(list.result$TN) / (sum(list.result$TN) + sum(list.result$FP))
+    
+    TSS <-
+      sensitivity + specificity - 1
+    
+    # IoU calculation
+    IoU <-
+      sum(list.result$TP) / (sum(list.result$TP) + sum(list.result$FP) + sum(list.result$FN))
+  
+    TSS_IoU <- c(TSS, IoU)
+    names(TSS_IoU) <-  c("TSS", "IoU")
+    return(TSS_IoU)
+    }
