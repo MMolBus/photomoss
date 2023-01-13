f083768e3f52e81f76a304b5cde0e69340dc750e
MMolBus <36168270+MMolBus@users.noreply.github.com>
2023-01-13 12:13:19 +0100

    change tif.path for wd.path

--- R/photomoss.example.r
+++ R/photomoss.example.r
@@ -4,17 +4,17 @@
 # 
 # 
 # # seleccionamos los colores en la carta de color para hacer la calibración
-# tif.path <- "path"
-# chart <- chart.from.tif(tif.path)
+# wd.path <- "path"
+# chart <- chart.from.tif(wd.path)
 # 
 # ################################################################################
 # 
 # # Importamos el área de interes que creamos con Image J
 # # y lo convertimos en un objeto del tipo SpatialPolygons
-# # roi.path <- paste0(tif.path, "/roi")
+# # roi.path <- paste0(wd.path, "/roi")
 # # roi.paths <- Sys.glob(paste0(roi.path, "*.roi"))[1:4]
 # roi.paths<- list.files(path = "./roi",pattern=".roi$",full.names = T)[1:4]
-# polys <- lapply(roi.paths, roi2polygon, tif.path)
+# polys <- lapply(roi.paths, roi2polygon, wd.path)
 # 
 # # # # solo para comprobar, ploteamos:
 # # lapply(polys, plot, add=T, col="red")
@@ -23,7 +23,7 @@
 # # mapply(text, centroids, gsub("o/", "", names(polys)), cex=0.55)
 # 
 # # seleccionamos los pixeles de interés que caen en nuestro polygono de interés
-# obs.areas <- lapply(polys, function(x){extractPIX.from.Poly(tif.path, x)})
+# obs.areas <- lapply(polys, function(x){extractPIX.from.Poly(wd.path, x)})
 # names(obs.areas) <- paste0("roi", c(1:length(obs.areas)))
 # # lapply(obs.dfs, head)
 # 
@@ -36,6 +36,6 @@
 # 
 # 
 # # FOUR obs.areas per picture
-# # ccSpectral.multiareas(tif.path, chart = chart, obs.areas = obs.dfs, ml = F, pdf=T)
+# # ccSpectral.multiareas(wd.path, chart = chart, obs.areas = obs.dfs, ml = F, pdf=T)
 # 
-# ccSpectral.multiareas(tif.path, chart = chart, obs.areas = obs.areas, ml = F, pdf=T)
+# ccSpectral.multiareas(wd.path, chart = chart, obs.areas = obs.areas, ml = F, pdf=T)
