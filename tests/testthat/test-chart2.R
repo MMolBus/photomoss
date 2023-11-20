96096c1a5960fd633d0cea2b0ec1085bf10459c2
MMolBus <36168270+MMolBus@users.noreply.github.com>
2023-11-20 13:23:11 +0100

    test-chart2 updated

--- tests/testthat/test-chart2.R
+++ tests/testthat/test-chart2.R
@@ -1,182 +1,72 @@
-# test-chart2 
+# test-chart2
 
-
-
-# data <- data.frame(Names = c("Pippin", "Merry"))
-system.file("data", "tiff", package = "PhotomossR") |> list.files()
-
-getwd()
-
-work_dir <- "./data/tiff" 
-
-setwd(work_dir)
-getwd()
-
-chart <-
-
-      chart2(pic.path = "./vis", pic.format = "tif")
-
-
-path <- tempdir()
-
+# Load necessary libraries for testing
 library(testthat)
-library(mockr)
-# Micro test
+library(devtools)
 
+# Load the package for testing
 devtools::load_all()
 
-setwd("..")
-getwd()
-
-
-# Define the test
+# Define the test for the chart2 function
 test_that("chart2 function", {
-      # Override the locator function for testing
-      original_locator <- graphics::locator
-      locator <- function(n = 1, type = "p") {
-            coords <- list(x = runif(n), y = runif(n))
-            return(coords)
-      }
+      # Load a TIFF picture for testing
+      picture <- tiff::readTIFF(list.files(
+            path = pic.path,
+            pattern = ".tif$",
+            full.names = T
+      )[1])
+      
+      # Get raster picture dimensions for creating a doomy tile.coords dataframe
+      pic_raster <- terra::rast(picture)
       
       # Set parameters for chart2 function
-      pic.path <- "data/tiff/vis"
-      samp.width <- 0.01
-      pic.format <- "tif"
-      xriteclassic.chart <- TRUE
-      n.color.tiles <- NULL
-      chart.coords <- list(x = runif(24), y = runif(24))
-
+      pic_path <- "data/tiff/vis"
+      samp_width <- 40
+      interactive <- FALSE
+      tile_coords <-
+            data.frame(
+                  x = round(runif(24), 2) * dim(pic_raster)[2],
+                  y = round(runif(24), 2) * dim(pic_raster)[1]
+            )
+      xriteclassic_chart <- TRUE
+      n_color_tiles <- NULL
+      
+      # If tif images-----------------------------------------------------------
+      pic_format <- "tif"
+      
       # Call the chart2 function
-      result <- chart2(pic.path, samp.width, pic.format, xriteclassic.chart, n.color.tiles)
+      result <- chart2(
+            pic.path = pic_path,
+            samp.width = samp_width,
+            pic.format = pic_format,
+            interactive = FALSE ,
+            xriteclassic.chart = xriteclassic_chart,
+            n.color.tiles = n_color_tiles,
+            tile.coords = tile_coords
+      )
       
       # Test the result
       expect_s4_class(result, "SpatVector")
-      expect_equal(length(result), ifelse(xriteclassic.chart, 24, n.color.tiles))
-      
-      # Restore the original locator function
-      locator <- original_locator
-})
-
-
-
-
-pic.path <- "data/tiff/vis"
-samp.width <- 0.01
-pic.format <- "tif"
-xriteclassic.chart <- TRUE
-n.color.tiles <- NULL
-
-local({
-      # Override the locator function to return predefined coordinates
-      local_mock(
-            original_locator <- locator,
-            locator <- function(n = 1, type = "p") {
-                  coords <- list(x = runif(n), y = runif(n))
-                  return(coords)}
-            # locator = function(n = n.color.tiles , type = "p")
-            #       list(x = round(runif(
-            #             length(n.color.tiles), 0, 1
-            #       ), 2),
-            #       y = round(runif(
-            #             length(n.color.tiles), 0, 1
-            #       ), 2))
-      )
-      #
-      # test_that("test chart function", {
-      #       # Your test code here
-      # })
-      #
-      pic.path <- "data/tiff/vis"
-      samp.width <- 0.01
-      pic.format <- "tif"
-      xriteclassic.chart <- TRUE
-      n.color.tiles <- NULL
-      
-      result <- 
-            chart2(pic.path, samp.width, pic.format, xriteclassic.chart, n.color.tiles)
-      
-      expect_s4_class(result, "SpatialPolygons")
       expect_equal(length(result),
-                   ifelse(xriteclassic.chart, 24, n.color.tiles))
-      # Restore the original locator function
-      locator <- original_locator
+                   ifelse(xriteclassic_chart, nrow(tile_coords), n_color_tiles))
+      
+      # If jpg images-----------------------------------------------------------
+      pic_format <- "jpeg"
+      pic_path <- "data/tiff/jpeg"
+      
+      # Call the chart2 function
+      result <- chart2(
+            pic.path = pic_path,
+            samp.width = samp_width,
+            pic.format = pic_format,
+            interactive = FALSE ,
+            xriteclassic.chart = xriteclassic_chart,
+            n.color.tiles = n_color_tiles,
+            tile.coords = tile_coords
+      )
+      
+      # Test the result
+      expect_s4_class(result, "SpatVector")
+      expect_equal(length(result),
+                   ifelse(xriteclassic_chart, nrow(tile_coords), n_color_tiles))
 })
-
-result <- chart2(pic.path, samp.width, pic.format, xriteclassic.chart, n.color.tiles)
-class(result)
-# expect_is(result, "SpatialPolygonsDataFrame")
-expect_s4_class(result, "SpatialPolygons")
-expect_equal(length(result), ifelse(xriteclassic.chart, 24, n.color.tiles))
-
-
-      # Define your test
-      test_that("chart2 function works correctly", {
-            # Set up any necessary variables or data
-# test_that("PDF certificates are created", {
-      
-      skip_on_ci()
-      skip_on_cran()
-      
-      # Load the necessary library
-
-      
-            pic.path <- "path/to/your/test/image"
-            samp.width <- 0.01
-            pic.format <- "jpg"
-            xriteclassic.chart <- TRUE
-            n.color.tiles <- NULL
-            
-            # Call your function with the test data
-            result <- chart2(pic.path, samp.width, pic.format, xriteclassic.chart, n.color.tiles)
-            
-            # Check that the result is as expected
-            expect_is(result, "SpatialPolygonsDataFrame")
-            expect_equal(length(result), ifelse(xriteclassic.chart, 24, n.color.tiles))
-      })
-      
-      
-      
-       
-      # ## Spanish
-      # create_certificate_attendance(data = data, path = path, type = "Adventure",
-      #                               title = "Going to Mordor",
-      #                               hours = "1000", name.column = "Names",
-      #                               language = "Spanish", signer = "Gandalf",
-      #                               date = "10/07/3064"
-      # )
-      # 
-      # expect_true(file.exists(file.path(path, "Asistencia_Pippin.pdf")))
-      # expect_true(file.exists(file.path(path, "Asistencia_Merry.pdf")))
-      # 
-      # 
-      # ## English
-      # create_certificate_attendance(data = data, path = path, type = "Adventure",
-      #                               title = "Going to Mordor",
-      #                               hours = "1000", name.column = "Names",
-      #                               language = "English", signer = "Gandalf",
-      #                               date = "10/07/3064"
-      # )
-      # 
-      # expect_true(file.exists(file.path(path, "Attendance_Pippin.pdf")))
-      # expect_true(file.exists(file.path(path, "Attendance_Merry.pdf")))
-      
-      
-# })
-
-
-# test_that("Rmd file is present when keep.files = TRUE", {
-#       
-#       skip_on_ci()
-#       skip_on_cran()
-#       
-#       create_certificate_attendance(data = data, path = path, type = "Adventure",
-#                                     title = "Going to Mordor",
-#                                     hours = "1000", name.column = "Names",
-#                                     language = "English", signer = "Gandalf",
-#                                     date = "10/07/3064",
-#                                     keep.files = TRUE
-#       )
-#       
-#       expect_true(file.exists(file.path(path, "attendance.Rmd")))
-#       
-# })
